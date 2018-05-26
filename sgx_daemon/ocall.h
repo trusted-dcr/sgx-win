0a7857d7dbaabeaf1b5e2d35ad67a0d5528c3b02
Malthe Kirkbro <maek@itu.dk>
2018-05-26 16:18:32 +0200

    sgx_daemon.cpp/h -> ocall.cpp/h

---
+++ sgx_daemon/ocall.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "msg.h"
+
+void request_time();
+void send_command_req(command_req_t req);
+void send_command_rsp(command_rsp_t rsp);
+void send_append_req(append_req_t req);
+void send_append_rsp(append_rsp_t rsp);
+void send_poll_req(poll_req_t req);
+void send_poll_rsp(poll_rsp_t rsp);
+void send_election_req(election_req_t req);
+void send_election_rsp(election_rsp_t rsp);
--- sgx_daemon/sgx_daemon.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-//void send_msg(void* ptr, size_t msg_size);
