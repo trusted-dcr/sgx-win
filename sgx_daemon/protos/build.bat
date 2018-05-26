0ba5620ae2ec2173a5d29ab0e47f3a4bb328ebb4
Malthe Kirkbro <maek@itu.dk>
2018-05-26 15:59:51 +0200

    Move pre-build event to protos/build.bat
    Update tdcr-proto

---
+++ sgx_daemon/protos/build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set PROTOC=protoc
+set GRPC="C:\Program Files\vcpkg\installed\x86-windows\tools\grpc\grpc_cpp_plugin.exe"
+
+set SOLUTION_DIR=..\..
+set PROJECT_DIR=..
+
+%PROTOC%^
+  --proto_path=%SOLUTION_DIR%\tdcr-proto\protos^
+  --cpp_out=%PROJECT_DIR%\protos^
+  --grpc_out=%PROJECT_DIR%\protos^
+  --plugin=protoc-gen-grpc=%GRPC%^
+    %SOLUTION_DIR%\tdcr-proto\protos\crypto.proto^
+    %SOLUTION_DIR%\tdcr-proto\protos\network.proto^
+    %SOLUTION_DIR%\tdcr-proto\protos\raft.proto^
+    %SOLUTION_DIR%\tdcr-proto\protos\sgxd.proto
